2c2fabe0b37ad4b558f05eabc6ff6a144aeec74a
Murilo Morais <moraisgomes@discente.ufg.br>
2025-05-14 22:43:28 +0000

    commitado

---
+++ main/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"INTRODUCAO_BACKEND/conn"
+	"fmt"
+)
+
+func main() {
+	database, err:=conn.ConnectDatabase()
+
+	if err!=nil{
+		panic(err
+	}
+
+	query, _:=database.Prepare("INSERT INTO aluno(nome, curso) VALUES ($1, $2)")
+
+	query.Exec("Murilo", "Enegnharia de Software")
+
+	rows,_:=database.Query("SELECT id, nome, ncurso FROM aluno")
+
+	for rows.Next(){
+		var (
+			nome, curso string
+			id int
+		)
+
+		rows.Scan(
+				&id,
+				&nome,
+				&curso,
+		)
+
+		fmt.Println("---------------------------------------------------------")
+		fmt.Printf("ID: %d\nNome: %s\nCurso: %s\n", id, nome, curso)
+	}
+
+	updateQuery, _:=database.Prepare("DELETE FROM aluno WHERE id = $1")
+
+	deleteQuery.Exec(2)
+
+	fmt.Println("Aluno com ID 2 removido com sucesso.")
+}
