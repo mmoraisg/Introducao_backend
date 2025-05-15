0dc932e11650d0bfad8608011070aa73d1d72eae
mmoraisg <moraisgomes@discente.ufg.br>
2025-05-15 20:00:50 -0300

    commitado

--- main/main.go
+++ main/main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-	"INTRODUCAO_BACKEND/conn"
+	"Introducao_backend/conn"
 	"fmt"
 )
 
 func main() {
-	database, err:=conn.ConnectDatabase()
+	database, err := conn.ConnectDatabase()
 
-	if err!=nil{
-		panic(err
+	if err != nil {
+		panic(err)
 	}
 
-	query, _:=database.Prepare("INSERT INTO aluno(nome, curso) VALUES ($1, $2)")
+	query, _ := database.Prepare("INSERT INTO aluno(nome, curso) VALUES ($1, $2)")
 
 	query.Exec("Murilo", "Enegnharia de Software")
 
-	rows,_:=database.Query("SELECT id, nome, ncurso FROM aluno")
+	rows, _ := database.Query("SELECT id, nome, ncurso FROM aluno")
 
-	for rows.Next(){
+	for rows.Next() {
 		var (
 			nome, curso string
-			id int
+			id          int
 		)
 
 		rows.Scan(
-				&id,
-				&nome,
-				&curso,
+			&id,
+			&nome,
+			&curso,
 		)
 
 		fmt.Println("---------------------------------------------------------")
 		fmt.Printf("ID: %d\nNome: %s\nCurso: %s\n", id, nome, curso)
 	}
 
-	updateQuery, _:=database.Prepare("DELETE FROM aluno WHERE id = $1")
+	updateQuery, _ := database.Prepare("DELETE FROM aluno WHERE id = $1")
 
 	deleteQuery.Exec(2)
 
 	fmt.Println("Aluno com ID 2 removido com sucesso.")
 
-	deleteQuery, _:=database.Prepare("DELETE FROM aluno WHERE id = $1")
+	deleteQuery, _ := database.Prepare("DELETE FROM aluno WHERE id = $1")
 
 	deleteQuery.Exec(2)
 
